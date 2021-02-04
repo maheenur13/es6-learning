3d55986d1b05977efb6e1d601d22592f4083bf1a
Jahidun nur <differentmahee13@gmail.com>
2021-02-04 21:30:33 +0600

    adding

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
-let sumIt = (num1,num2)=>sum = num1+num2;
+let sumIt = (num1,num2)=>{
+    // sum = num1+num2;
+    if (num1==num2){
+        console.log(`${num1} and ${num2} are equal`);
+    }
+    else if(num1>num2){
+        console.log(`${num1} is bigger than ${num2}`);
+    }
+    else if(num1<num2){
+        console.log(`${num2} is bigger than ${num1}`);
+    }
+}
 let newSum= sumIt(12,13);
-console.log(newSum);
+// console.log(newSum);
---
+++ trippledot.js
@@ -0,0 +1,7 @@
+const arrayone = [45,54,34,66,65];
+const arraytwo = [67,43,56,76,34];
+const joinArryIntoArray = [...arrayone,...arraytwo,67];
+console.log(joinArryIntoArray);
+const money = [567,345,765,786];
+const maxAmount = Math.max(...money);
+console.log(maxAmount);
