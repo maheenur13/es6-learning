9cb2b97d87cb8d4e8a72e403ba84a37fac0bbf12
Jahidun nur <differentmahee13@gmail.com>
2021-02-04 16:56:59 +0600

    practice

---
+++ class.js
@@ -0,0 +1,10 @@
+class student{
+    constructor(sAge,sName,sUni){
+        this.age = sAge;
+        this.name = sName;
+        this.uni = sUni;
+    }
+}
+const student1 = new student(22,"Jahidun Nur Mahee","Daffodil International University");
+const student2 = new student(20,"Israt Zaman Anonna","Daffodil International University");
+console.log(student1,student2);
---
+++ js.js
@@ -0,0 +1,3 @@
+let sumIt = (num1,num2)=>sum = num1+num2;
+let newSum= sumIt(12,13);
+console.log(newSum);
