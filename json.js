d73e3d5ff819eb1ce731fd5f0375a3a24a21d34a
Jahidun nur <differentmahee13@gmail.com>
2021-02-06 18:19:28 +0600

    json learnign

---
+++ json.js
@@ -0,0 +1,16 @@
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight format for storing and transporting data
+// JSON is often used when data is sent from a server to a web page
+// JSON is "self-describing" and easy to understand
+
+
+
+// Exchanging Data
+// When exchanging data between a browser and a server, the data can only be text.
+// JSON is text, and we can convert any JavaScript object into JSON, and send JSON to the server.
+// We can also convert any JSON received from the server into JavaScript objects.
+// This way we can work with the data as JavaScript objects, with no complicated parsing and translations.
+
+
+// Sending Data
+// If you have data stored in a JavaScript object, you can convert the object into JSON, and send it to a server:
